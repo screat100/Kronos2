523d6feddda291eb036521fe0ab80e1ba3b5267e
screat100 <61044260+screat100@users.noreply.github.com>
2021-02-16 16:22:10 +0900

    add effect asset

---
+++ Kronos/Assets/Resources/Effect/Epic Toon FX/Demo/Scripts/FireworkFX.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EpicToonFX
+{
+    public class FireworkFX : MonoBehaviour
+    {
+        public GameObject chosenEffect;
+        //public float loopTimeLimit = 2.0f;
+
+        [Header("Spawn without")]
+
+        public bool spawnWithoutLight = true;
+        public bool spawnWithoutSound = true;
+
+        void Start()
+        {
+            //PlayEffect();
+        }
+
+        public void PlayEffect()
+        {
+            GameObject effectPlayer = (GameObject)Instantiate(chosenEffect, transform.position, transform.rotation);
+
+            if (spawnWithoutLight = true && effectPlayer.GetComponent<Light>())
+            {
+                effectPlayer.GetComponent<Light>().enabled = false;
+                //Destroy(gameObject.GetComponent<Light>());
+            }
+
+            if (spawnWithoutSound = true && effectPlayer.GetComponent<AudioSource>())
+            {
+                effectPlayer.GetComponent<AudioSource>().enabled = false;
+                //Destroy(gameObject.GetComponent<AudioSource>());
+                //StartCoroutine("EffectLoop");
+            }
+            //Destroy(effectPlayer);
+        }
+    }
+}
