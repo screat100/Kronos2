73a22cac65991521b4cf91dde26138a92c448440
G-Man-git <61372684+G-Man-git@users.noreply.github.com>
2021-02-24 22:08:51 +0900

    채팅 스킵 기능 추가 및 버그수정
    <대화 스킵>
    -대화 스크립트 진행 중, f키를 누르면 진행 중인 문장이 바로 완성됨
    <버그 수정>
    -대화 스크립트 진행 중, 이전 대사가 마치지 않았을 때 f키를 누르면 겹쳐서 나오는 현상 수정

--- Kronos/Assets/ChatScript.cs
+++ Kronos/Assets/ChatScript.cs
@@ -18,6 +18,7 @@ public class ChatScript : MonoBehaviour
     //대화 스크립트 할수있는지 없는지
     bool flag=true;
 
+    bool skip = false;
     string[] lines;
     int count=0;
     // Start is called before the first frame update
@@ -29,28 +30,39 @@ public class ChatScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Chat());
+        Chat();
     }
    
-    IEnumerator Chat()
+    void Chat()
     {
-        Chat_Text.text = "";
-        if (flag == true && Input.GetKeyDown(KeyCode.F) && lines[count]!=null)
+        if(flag ==true && Input.GetKeyDown(KeyCode.F) && lines.Length > count && skip == true)
         {
-            yield return StartCoroutine(Typing_chat(gameObject.name, lines[count]));
+            Chat_Text.text = "";
+            Chat_Text.text = lines[count];
+            skip = false;
+        }
+        else if (flag == true && Input.GetKeyDown(KeyCode.F) && lines.Length >count&&skip==false)
+        {
+            StartCoroutine(Typing_chat(gameObject.name, lines[count]));
             Debug.Log(lines[count]);
-            count++;
         }
     }
     IEnumerator Typing_chat(string narrator, string chat)
     {
         //CharacterName.text = narrator;
-
-        for(int i =0;i<chat.Length;i++)
+        Chat_Text.text = "";
+        for (int i =0;i<chat.Length;i++)
         {
+            skip = true;
             Chat_Text.text += chat[i];
             yield return new WaitForSeconds(0.1f);
+            if (skip == false)
+            {
+                break;
+            }
         }
+        skip = false;
+        count++;
     }
 
 
